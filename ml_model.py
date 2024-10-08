0939d83ef137e1a7f83ca7fbf477f5a64b17f09e
Shivani-Tej <smurde29@gmail.com>
2024-10-08 22:32:28 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,12 @@
 print("adding all ml related code")
+
+def model1():
+    print("adding code for model1")
+
+
+def model2():
+    print("adding code for model2")
+
+
+def model3():
+    print("adding code for model3")
